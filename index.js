f1879cadebd604a02c36d0336ad82a5c0368271f
MD Ariful Islam Shanto <arifulislam1077@gmail.com>
2023-12-22 00:37:58 +0600

    Added a delete method for deleting the single todo

--- index.js
+++ index.js
@@ -87,6 +87,14 @@ async function run() {
       const result = await todoCollection.updateOne(query,updatedDoc);
       res.send(result)
     })
+
+    //? Delete todo
+    app.delete('/deleteTodo/:id' , async(req, res) => {
+      const id = req.params.id;
+      const query = { _id : new ObjectId(id) };
+      const result = await todoCollection.deleteOne(query);
+      res.send(result);
+    })
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
